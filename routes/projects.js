2d2e84178cad39fc4ecd835d54bba839f24572f4
Marshall G. Gates <mggates@gmail.com>
2024-07-30 15:58:44 -0400

    Setup the refactor of projects

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('projectlist', { title: 'Projects'});
+  res.render('project/list', { title: 'Projects'});
 });
 
 module.exports = router;
