61fea8d0fb2d751f72949e5ed7062367ff4105d6
Marshall G. Gates <mggates@gmail.com>
2024-10-02 09:32:15 -0400

    fixed confusiong in check list field names

--- routes/components.js
+++ routes/components.js
@@ -9,6 +9,10 @@ router.get('/', async function(req, res, next) {
   res.render('component_type/list', { title: 'Component Types', component_types: data });
 });
 
+router.get('/new', async function(req, res, next) {
+  res.redirect('/component_types/');
+});
+
 /* GET item page */
 router.get('/:id', async function(req, res, next) {
   const id = req.params.id;
@@ -25,9 +29,10 @@ router.get('/edit/:id', async function(req, res, next) {
   res.render('component_type/edit', {title: 'Component Type', component_type: data, package_types: packs});
 })
 
+/* POST existing item update */
 router.post('/:id', async function( req, res, next) {
   const id = req.params.id;
-  await updateComponentType(id, req.body.description, req.body.symbol, req.body.table_name, req.body.package_types)
+  await updateComponentType(id, req.body.description, req.body.symbol, req.body.table_name, req.body.package_type_selection)
   res.redirect('/component_types/'+id);
 })
 
