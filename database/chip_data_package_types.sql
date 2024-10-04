6b34991a064499858ac2d1c3a65f97328c27312c
Marshall G. Gates <mggates@gmail.com>
2024-10-04 17:44:40 -0400

    Improve the database backup for better change tracking

---
+++ database/chip_data_package_types.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `chip_data` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `chip_data`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: chip_data
+-- ------------------------------------------------------
+-- Server version	8.0.39-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `package_types`
+--
+
+DROP TABLE IF EXISTS `package_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package_types` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `description` varchar(32) NOT NULL,
+  `mounting_type_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type_mounting_type_idx` (`mounting_type_id`),
+  CONSTRAINT `package_types_ibfk_1` FOREIGN KEY (`mounting_type_id`) REFERENCES `mounting_types` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_types`
+--
+
+LOCK TABLES `package_types` WRITE;
+/*!40000 ALTER TABLE `package_types` DISABLE KEYS */;
+INSERT INTO `package_types` VALUES (1,'DIP','Dual In-Line Package',1),(2,'Axial','Axial',1),(3,'Radial','Radial',1),(4,'PLCC','Plastic Leaded Chip Carrier',2),(5,'Chassis','Chassis',4),(6,'SIP','Single In-line Package',1),(7,'SMD','Surface Mount Device',3),(8,'MELF','Metal Electrode Leadless Face',3),(9,'SOIC','Small Outline Integrated Circuit',3),(10,'SOP','Small Outline Package',3),(11,'SOT','Small Outline Transistor',3),(12,'SOD','Small Outline Diode',3),(13,'QFP','Quad Flat Package',3),(14,'QFN','Quad Flat No-Leads Package',3),(15,'THD','Through Hole Device',1),(16,'QIP','Quad in-line package',1),(17,'TO-XX','Transistor Outline',1),(18,'Clamp','Fuse Clamp',1);
+/*!40000 ALTER TABLE `package_types` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-03 21:19:42
