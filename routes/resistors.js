d821d8b3bef2da8c06dfe41abd639f42f46c9149
Marshall G. Gates <mggates@gmail.com>
2024-11-09 20:53:49 -0500

    Add color bands to resistor pin diagram

--- routes/resistors.js
+++ routes/resistors.js
@@ -1,12 +1,82 @@
 var express = require('express');
 var router = express.Router();
 const {  getResistor, getPins, getDipLeftPins, getDipRightPins, 
-  getPllcLeftPins, getPllcRightPins, getPllcTopPins, getPllcBottomPins, 
-  getQuadLeftPins, getQuadRightPins, getQuadTopPins, getQuadBottomPins,
   getSpecs, getNotes, getAliases, createAlias, deleteAliases, createResistor, updateResistor, createPin, updatePin,
   getInventoryByComponentList, getPackageTypesForComponentType, getComponentSubTypesForComponentType,
-  getPickListByName, getComponentType, getComponentTypeList } = require('../database');
+  getPickListByName, getListEntry, getComponentType, getComponentTypeList } = require('../database');
 const {parse_symbol} = require('../utility');
+
+function getResistorBands(resistance, unit_modifier, tolerance) {
+  const colorCodes = [
+    "black", "brown", "red", "orange", "yellow", 
+    "green", "blue", "violet", "grey", "white",
+    "gold", "silver", "none"
+  ];
+
+  let resistanceStr = resistance.toString();
+  let multiplier = Math.pow(10, unit_modifier);
+
+  let resistanceValue = parseFloat(resistanceStr) * multiplier;
+  const significantDigits = resistanceValue.toString().split('.')[0];
+
+  let wholeNumber = significantDigits[0];
+
+  // Handle decimal values
+  if (significantDigits.length > 1) {
+    wholeNumber += significantDigits[1]; 
+    wholeNumber = parseInt(wholeNumber);
+  }
+
+  // Calculate multiplier for band
+  multiplier = 1;
+  while (resistanceValue >= 100) {
+    resistanceValue /= 10;
+    multiplier *= 10;
+  }
+
+  let bands = [];
+  
+  // First two bands (significant digits)
+  for (let i = 0; i < 2; i++) {
+    bands.push(colorCodes[parseInt(wholeNumber.toString()[i])]);
+  }
+
+  // Third band (multiplier)
+  let multiplierExponent = Math.log10(multiplier);
+  bands.push(colorCodes[multiplierExponent]);
+
+  bands.push(colorCodes[12]);  // Blank band for spacing
+
+  // Fourth band (tolerance)
+  switch (tolerance) {
+    case 1:
+      bands.push(colorCodes[1]);
+      break;
+    case 2:
+      bands.push(colorCodes[2]);
+      break;
+    case 0.5:
+      bands.push(colorCodes[5]);
+      break;
+    case 0.25:
+      bands.push(colorCodes[6]);
+      break;
+    case 0.1:
+      bands.push(colorCodes[7]);
+      break;
+    case 5:
+      bands.push(colorCodes[10]);
+      break;
+    case 10:
+      bands.push(colorCodes[11]);
+      break;
+    default:
+      bands.push(colorCodes[12]);
+  }
+
+  return bands;
+}
+
 
 /* GET new item page */
 router.get('/new', async function(req, res, next) {
@@ -56,20 +126,13 @@ router.get('/:id', async function(req, res, nest) {
   const pins = await getPins(id);
   const dip_left_pins = await getDipLeftPins(id);
   const dip_right_pins = await getDipRightPins(id);
-  const plcc_left_pins = await getPllcLeftPins(id);
-  const plcc_right_pins = await getPllcRightPins(id);
-  const plcc_top_pins = await getPllcTopPins(id);
-  const plcc_bottom_pins = await getPllcBottomPins(id);
-  const quad_left_pins = await getQuadLeftPins(id);
-  const quad_right_pins = await getQuadRightPins(id);
-  const quad_top_pins = await getQuadTopPins(id);
-  const quad_bottom_pins = await getQuadBottomPins(id);
   const specs = await getSpecs(id);
   const notes = await getNotes(id);
   const inventory = await getInventoryByComponentList(id);
   const aliases = await getAliases(id);
   const component_types = await getComponentTypeList();
   const component_type_id = data.component_type_id;
+  const units = await getListEntry(data.unit_id);
 
   fixed_pins = [];
   iswide = 'dpindiagram';
@@ -83,89 +146,20 @@ router.get('/:id', async function(req, res, nest) {
   });
 
   layout_pins = [];
-  top_pins = [];
-  bottom_pins = [];
-  if (data.package == 'PLCC') {
-    i = 0;
-    plcc_left_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&nbsp;&#9679;'
-      } else {
-        bull = ''
-      }
-      layout_pins.push(
-        {'left_pin': pin.pin_number, 'bull': bull, 'right_pin': plcc_right_pins[i].pin_number, 
-        'left_sym': parse_symbol(pin.pin_symbol), 'right_sym': parse_symbol(plcc_right_pins[i].pin_symbol),
-      });
-      i++;
+  bands = getResistorBands(data.resistance, units.modifier_value, data.tolerance) ;
+  i = 0;
+  dip_left_pins.forEach(function(pin) {
+    if (pin.pin_number == 1) {
+      bull = '&nbsp;&#9679;'
+    } else {
+      bull = ''
+    }
+    layout_pins.push(
+      {'left_pin': pin.pin_number, 'bull': bull, 'right_pin': dip_right_pins[i].pin_number, 
+      'left_sym': parse_symbol(pin.pin_symbol), 'right_sym': parse_symbol(dip_right_pins[i].pin_symbol),
     });
-
-    plcc_top_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&#9679;&nbsp;'
-      } else {
-        bull = ''
-      }
-      top_pins.push({'pin': pin.pin_number, 'bull': bull, 'sym': parse_symbol(pin.pin_symbol)});
-    });
-
-    plcc_bottom_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&#9679;&nbsp;'
-      } else {
-        bull = ''
-      }
-      bottom_pins.push({'pin': pin.pin_number, 'bull': bull, 'sym': parse_symbol(pin.pin_symbol)});
-
-    });
-  } else if  ((data.package == 'QFN') || (data.package == 'QFP')) {
-    i = 0;
-    quad_left_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&#9679;'
-      } else {
-        bull = ''
-      }
-      layout_pins.push(
-        {'left_pin': pin.pin_number, 'bull': bull, 'right_pin': quad_right_pins[i].pin_number, 
-        'left_sym': parse_symbol(pin.pin_symbol), 'right_sym': parse_symbol(quad_right_pins[i].pin_symbol),
-      });
-      i++;
-    });
-
-    quad_top_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&#9679;&nbsp;'
-      } else {
-        bull = ''
-      }
-      top_pins.push({'pin': pin.pin_number, 'bull': bull, 'sym': parse_symbol(pin.pin_symbol)});
-    });
-
-    quad_bottom_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&#9679;&nbsp;'
-      } else {
-        bull = ''
-      }
-      bottom_pins.push({'pin': pin.pin_number, 'bull': bull, 'sym': parse_symbol(pin.pin_symbol)});
-
-    });
-  } else {
-    i = 0;
-    dip_left_pins.forEach(function(pin) {
-      if (pin.pin_number == 1) {
-        bull = '&nbsp;&#9679;'
-      } else {
-        bull = ''
-      }
-      layout_pins.push(
-        {'left_pin': pin.pin_number, 'bull': bull, 'right_pin': dip_right_pins[i].pin_number, 
-        'left_sym': parse_symbol(pin.pin_symbol), 'right_sym': parse_symbol(dip_right_pins[i].pin_symbol),
-      });
-      i++;
-    });
-  }
+    i++;
+  });
 
   clean_specs = [];
   specs.forEach(function(spec) {
@@ -182,7 +176,7 @@ router.get('/:id', async function(req, res, nest) {
   })
 
   res.render('resistor/detail', { title: data.chip_number + ' - ' + data.description, data: data, 
-    pins: fixed_pins, layout_pins: layout_pins, top_pins: top_pins, bottom_pins: bottom_pins,
+    pins: fixed_pins, layout_pins: layout_pins, top_pins: bands,
     specs: clean_specs, notes: clean_notes, aliases: aliases, inventory: inventory,
     component_types: component_types, component_type_id: component_type_id });
 });
