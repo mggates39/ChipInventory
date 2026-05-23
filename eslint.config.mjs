fb8252152adc16fa08f44c8967a9f131b0074d13
Marshall G. Gates <mggates@gmail.com>
2026-05-22 20:40:27 -0400

    WIre in a pretty proint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,12 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.js"], plugins: {pluginReact}, languageOptions: { globals: {  ...globals.node, }, }, },
-
+  eslintConfigPrettier,
 ]);
 
